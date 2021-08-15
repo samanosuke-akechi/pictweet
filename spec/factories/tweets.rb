7cd2202cc2053a0748e52995d07fb9edbd6c2e7e
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-08-15 19:45:24 +0900

    tweetのFactoryBotの画像の生成をランダム文字列から実際にWEBにある画像URLに変更

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :tweet do
     text {Faker::Lorem.sentence}
-    image {Faker::Lorem.sentence}
+    image {"https://static.retrip.jp/spot/e5c7b040-2956-4104-9fc6-db6f498e63d7/images/2c147da2-0684-4ebb-abf1-731a5ef8a9a8_l.jpg"}
     association :user
   end
 end
